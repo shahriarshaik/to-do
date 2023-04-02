2feecc697de2dae6f48b02f7959ade8720514894
ShakeZpear <meherss@uio.no>
2023-04-02 03:55:47 +0200

    noe

--- to-do/src/components/TodoForm.js
+++ to-do/src/components/TodoForm.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const TodoForm = () => {
+        const [Value, setValue] = useState("");
     return(
-        <div>TodoForm</div>
+        <form className='TodoForm'> 
+            <input type='text' className='todo-input' placeholder='What is the task today?' onChange={(e) => console.log(e.target.value)}/> 
+            <button className='todo-button' type='submit'> Add task</button>
+        </form>
     )
 }
